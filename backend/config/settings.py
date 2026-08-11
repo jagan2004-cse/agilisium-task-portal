a3622784e7f415625f46e41d1a30fd961ba6955d
jagan2004 <jagan2004@gmail.com>
2026-08-11 17:03:20 +0530

    Add DEFAULT_FILE_STORAGE and querystring_auth config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -124,12 +124,15 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-south-1')
 
 if AWS_STORAGE_BUCKET_NAME and AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = False
+    
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
     STORAGES = {
         "default": {
@@ -141,6 +144,7 @@ if AWS_STORAGE_BUCKET_NAME and AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
                 "region_name": AWS_S3_REGION_NAME,
                 "default_acl": None,
                 "file_overwrite": False,
+                "querystring_auth": False,
             },
         },
         "staticfiles": {
